88cf1bf752fb93b345674ac7fe241b5fb0e6689c
Damian Posener <750763+deains@users.noreply.github.com>
2023-09-03 17:09:34 +0100

    Version 2.1.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     project_urls={
         'Source': 'https://github.com/racetimeGG/racetime-bot',
     },
-    version='2.0.0',
+    version='2.1.0',
     install_requires=[
         'aiohttp',
         'asgiref',
